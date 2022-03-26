b150bdc5278ce65b6aa1c700df8065d145b546e9
cunharodrigues <pwcunha@gmail.com>
2022-03-26 11:39:52 -0300

    Redirecting to login when 401 or 403

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
+import history from './history';
 
 type LoginResponse = {
   access_token: string;
@@ -8,51 +9,82 @@ type LoginResponse = {
   scope: string;
   userFirstName: string;
   userId: number;
-}
+};
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
-  const tokenkey = 'authData';
+const tokenkey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_APP_CLIENT_SECRET ?? 'dscatalog123';
 
-
 type LoginData = {
   username: string;
   password: string;
-}
+};
 
 export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
     'Content-type': 'application/x-www-form-urlencoded',
-    Authorization : 'basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+    Authorization: 'basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
   };
 
-  const data = qs.stringify ({
-      ...loginData,
-      grant_type: 'password'
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
   });
 
-  return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers });
-}
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
+};
 
-export const requestBackend = (config:AxiosRequestConfig) => {
-  
-  const headers = config.withCredentials ? {
+export const requestBackend = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
         ...config.headers,
-        Authorization: "Bearer " + getAuthData().access_token
-  } : config.headers;
-  
-  return axios({...config, baseURL: BASE_URL, headers});
-}
+        Authorization: 'Bearer ' + getAuthData().access_token,
+      }
+    : config.headers;
 
-export const saveAuthData =(obj : LoginResponse) => {
-  localStorage.setItem('authData', JSON.stringify(obj));  
-}
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
+export const saveAuthData = (obj: LoginResponse) => {
+  localStorage.setItem('authData', JSON.stringify(obj));
+};
 
 export const getAuthData = () => {
-  const str = localStorage.getItem(tokenkey) ?? "{}";
+  const str = localStorage.getItem(tokenkey) ?? '{}';
   return JSON.parse(str) as LoginResponse;
-}
+};
+
+// Add a request interceptor
+axios.interceptors.request.use(
+  function (config) {
+    //console.log('INTERCEPTOR ANTES DA REQUISIÇÃO');
+    return config;
+  },
+  function (error) {
+    //console.log('INTERCEPTOR ERRO NA REQUISIÇÃO');
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    console.log('INTERCEPTOR RESPOSTA COM SUCESSO');
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      history.push('/admin/auth');
+    }
+    return Promise.reject(error);
+  });
