c7a965da743da6feb7815385169318a35860d632
cunharodrigues <pwcunha@gmail.com>
2022-03-26 17:53:34 -0300

    hasAnyRoles

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,15 +1,15 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
-import './styles.css';
 import { Link } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
+import { hasAnyRoles } from 'util/requests';
+
+import './styles.css';
+
 
 const Home = () => {
   return (
-    <div className="home-container">
-
-      <h1>{isAuthenticated() ? 'Autenticado' : 'Não autenticado'}</h1>
-
+    <div className="home-container">      
+        <h1>Resultado = {hasAnyRoles(['ROLE-ADMIN']) ? 'sim' : 'não'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -110,7 +110,31 @@ export const getTokenData = (): TokenData | undefined => {
   }
 };
 
-export const isAuthenticated = () => {
+export const isAuthenticated = (): boolean => {
+  let tokenData = getTokenData();
+  return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
+};
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+
+  if (roles.length === 0) {
+    return true;
+  }
+
   const tokenData = getTokenData();
-  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+
+  if (tokenData !== undefined) {
+    return roles.some(role => tokenData.authorities.includes(role));
+  }
+
+
+  /*if (tokenData !== undefined) {
+    for (var i = 0; i < roles.length; i++) {
+      if (tokenData.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+  }*/
+
+  return false;
 }
