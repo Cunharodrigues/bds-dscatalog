5a8f3858fa43de8d402a7e79b2ed83e16ad7087d
cunharodrigues <pwcunha@gmail.com>
2022-04-29 16:30:37 -0300

    Product list integration with event

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import Pagination from 'components/Pagination';
-import ProductFilter from 'components/ProductFilter';
+import ProductFilter, { ProductFilterData } from 'components/ProductFilter';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
 import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -12,6 +12,7 @@ import './styles.css';
 
 type ControlComponentsData = {
   activePage: number;
+  filterData: ProductFilterData;
 };
 
 const List = () => {
@@ -21,11 +22,16 @@ const List = () => {
     ControlComponentsData
   >({
     activePage: 0,
+    filterData: {name: "", category: null}
   });
 
   const handlePageChange = (pageNumber: number) => {
-    setControlComponentsData({activePage: pageNumber});
+    setControlComponentsData({activePage: pageNumber, filterData: controlComponentsData.filterData});
   };
+
+  const handleSubmitFilter = (data: ProductFilterData) => {
+    setControlComponentsData({activePage: 0, filterData: data });
+  }
 
   const getProducts = useCallback(() => {
     const config: AxiosRequestConfig = {
@@ -34,6 +40,8 @@ const List = () => {
       params: {
         page: controlComponentsData.activePage,
         size: 3,
+        name: controlComponentsData.filterData.name,
+        categoryId: controlComponentsData.filterData.category?.id
       },
     };
 
@@ -54,7 +62,7 @@ const List = () => {
             ADICIONAR
           </button>
         </Link>
-        <ProductFilter/>
+        <ProductFilter onSubmitFilter={handleSubmitFilter}/>
       </div>
       <div className="row">
         {page?.content.map((product) => (
@@ -67,6 +75,7 @@ const List = () => {
         ))}
       </div>
       <Pagination
+        forcePage={page?.number}
         pageCount={page ? page.totalPages : 0}
         range={3}
         onChange={handlePageChange}
