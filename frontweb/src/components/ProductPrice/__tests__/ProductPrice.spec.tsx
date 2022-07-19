7febb493f0ac333b6e9635cf087af25198097f1c
cunharodrigues <pwcunha@gmail.com>
2022-07-19 16:42:40 -0300

    ProductPrice, ProductCard tests

---
+++ frontweb/src/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import ProductPrice from "..";
+
+test('should render ProductPrice', () => {
+
+    // ARRENGE
+        const price = 10.1;
+    
+    // ACT
+
+        render(
+            <ProductPrice price={price} />
+        );
+
+        //screen.debug();
+
+    // ASSERT
+        expect(screen.getByText("R$")).toBeInTheDocument(); 
+        expect(screen.getByText("10,10")).toBeInTheDocument();   
+
+});
