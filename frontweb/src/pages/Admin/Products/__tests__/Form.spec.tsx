b94797b0bb6687ac182f510e8522cc466823e26e
cunharodrigues <pwcunha@gmail.com>
2022-07-27 14:46:37 -0300

    test should clear validation messages when filling out the form correctly

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -76,8 +76,6 @@ describe('Product Form create tests', () => {
             </Router>
         );
 
-        //screen.debug();
-
         // Simulação do Click do botao "SALVAR"
         const submitButton = screen.getByRole('button', { name: /salvar/i })
   
@@ -87,6 +85,47 @@ describe('Product Form create tests', () => {
                 const messages = screen.getAllByText('Campo obrigatório');
                 expect(messages).toHaveLength(5);
             });       
+    });
+
+
+    test('should clear validation messages when filling out the form correctly', async () => {
+
+        render(
+
+            <Router history={history}>                
+                <Form />
+            </Router>
+        );
+
+        // Simulação do Click do botao "SALVAR"
+        const submitButton = screen.getByRole('button', { name: /salvar/i })
+  
+        userEvent.click(submitButton);
+
+            await waitFor(() => {
+                const messages = screen.getAllByText('Campo obrigatório');
+                expect(messages).toHaveLength(5);
+            }); 
+        
+            const nameInput = screen.getByTestId("name");
+            const priceInput = screen.getByTestId("price");
+            const imgUrlInput = screen.getByTestId("imgUrl");
+            const descriptionInput = screen.getByTestId("description");
+            const categoriesInput = screen.getByLabelText("Categorias");
+        
+            // Simulação da caixa select
+            await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores']);
+    
+            // Simulação das caixa de texto
+            userEvent.type(nameInput, 'Computador');
+            userEvent.type(priceInput, '5000.12');
+            userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+            userEvent.type(descriptionInput, 'Computador muito bom');
+            
+            await waitFor(() => {
+                const messages = screen.queryAllByText('Campo obrigatório');
+                expect(messages).toHaveLength(0);
+            }); 
     });
 });
 
